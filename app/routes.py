92306c63c354c11f2c36042740fc82193ccff4dd
Arseni Shastaila <yoinezero.a@gmail.com>
2022-08-01 18:49:02 +0300

    add form view

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -21,4 +22,13 @@ def index():
             'body': 'Omae wa mou shindeiru!'
         },
     ]
-    return render_template('index.html', title='Blog', user=user, posts=posts)
+    return render_template('index.jinja2', title='Blog', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect('/')
+    return render_template('login.jinja2', title='Sign In', form=form)
