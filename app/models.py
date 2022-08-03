e50376f26fab4c73fa726d74099183b39889f84a
Arseni Shastaila <yoinezero.a@gmail.com>
2022-08-03 13:19:53 +0300

    Update user model

--- app/models.py
+++ app/models.py
@@ -11,6 +11,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return f"<User {self.username}>"
@@ -20,6 +22,9 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        return 'No avatar'
 
 
 class Post(db.Model):
