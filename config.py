a297ff4963ee8fca01bf2a4b2bfad38ca51062cf
Arseni Shastaila <yoinezero.a@gmail.com>
2022-08-03 15:55:19 +0300

    Add file and smtp logger for production

--- config.py
+++ config.py
@@ -4,7 +4,18 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
+    # SECRET
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you will never guess'
+
+    # DATABASE
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # MAIL
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT')
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = [os.environ.get('ADMINS', '').split(' ')]
