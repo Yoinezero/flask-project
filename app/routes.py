45eed3cf0629e216222a06542d4c9a9916cb66e4
Arseni Shastaila <yoinezero.a@gmail.com>
2022-08-04 21:42:29 +0300

    Implement message form

--- app/routes.py
+++ app/routes.py
@@ -4,8 +4,8 @@ from werkzeug.urls import url_parse
 from datetime import datetime
 
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
-from app.models import User
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
+from app.models import User, Post
 
 
 @app.before_request
@@ -15,12 +15,19 @@ def before_request():
         db.session.commit()
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    posts = []
-    return render_template('index.jinja2', title='Blog', posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    posts = current_user.get_actual_posts().all()
+    return render_template('index.jinja2', title='Blog', form=form, posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -73,10 +80,7 @@ def logout():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
+    posts = user.posts
     return render_template('user.jinja2', user=user, posts=posts)
 
 
